8350eb00a73a346b548caa73109d2cc73291338c
Okechukwu Ochia <emmirald@gmail.com>
2019-12-08 22:20:54 -0700

    replaced the description in the body

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,20 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+//$("#main").append("Okechukwu Ochia");
+var firstName = "Okechukwu";
+var lastName = "Ochia"
+var age = 28;
+console.log(firstName + " "+ lastName);
+
+var awesomeThoughts = "My name is "+firstName+" "+lastName+" and I am awesome!";
+console.log(awesomeThoughts);
+
+var email = "okechukwu.ochia@ucalgary.ca";
+var newEmail = email.replace("ucalgary.ca", "gmail.com");
+console.log(email);
+console.log(newEmail);
+
+var funThoughts = awesomeThoughts.replace("awesome", "fun");
+// console.log(funThoughts);
+$("#main").append(funThoughts);
